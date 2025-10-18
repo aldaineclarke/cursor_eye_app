a84c83662ed9049ec32a27b359ee9a71c0ce86e1
Kikira7258 <kikira7258@gmail.com>
2025-10-18 17:00:12 -0500

    Base form

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'intelli-light': '#E8F2FC',
+        'intelli-blue': '#3273AF',
+        'intelli-dark-blue': '#0A385F',
+        'intelli-dark': '#1C2227'
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      },
+      screens: {
+        'xs': '475px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+    },
+  },
+  plugins: [],
+}
+
