9bef47b502b9902283679f53dada6325996b4b75
Kikira7258 <kikira7258@gmail.com>
2025-10-18 16:53:20 -0500

    routes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/auth/login/login.component';
+import { SignUpComponent } from './views/auth/sign-up/sign-up.component';
+import { SettingsComponent } from './views/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
