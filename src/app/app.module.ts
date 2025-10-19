8e93d26ae155cdcd267f82dedce4eb2026150baa
Kikira7258 <kikira7258@gmail.com>
2025-10-19 00:22:10 -0500

    modified:   src/app/app.module.ts 	modified:   src/app/views/auth/login/login.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     BrowserAnimationsModule, // <-- Add this
     // NoopAnimationsModule,  // <-- OR use this if you want animations disabled
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
